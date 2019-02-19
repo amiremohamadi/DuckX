251b47a5f751bc5419c1e0bd35ba4a7c3a30bed2
amir <amiremohamadi@yahoo.com>
2019-02-19 12:50:08 +0330

    sample updated

--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <string>
-#include "../duckx.hpp"
-
+#include <"duckx.hpp">
+using namespace std;
 
 int main() {
     Document doc("my_test.docx");
     doc.open();
-    
 
-    for (auto p = doc.paragraphs(); p.hasNext() ; p.next()) {
-        std::cout << p.runs().text() << std::endl;
+    for (auto p = doc.paragraphs(); p.hasNext(); p.next()) {
+        for (auto r = p.runs(); r.hasNext(); r.next()) {
+            cout << r.text() << endl;
+        }
     }
 
     return 0;
