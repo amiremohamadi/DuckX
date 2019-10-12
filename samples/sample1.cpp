cd37d1dd27f233f885e92441fa01be600ea69598
AMIR <31338382+amiremohamadi@users.noreply.github.com>
2019-10-12 22:16:49 +0330

    fix sample.cpp bug

--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (auto p = doc.paragraphs(); p.has_next(); p.next()) {
         for (auto r = p.runs(); r.has_next(); r.next()) {
-            cout << r.tget_ext() << endl;
+            cout << r.get_ext() << endl;
         }
     }
 
