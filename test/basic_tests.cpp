275d02728633fd2c32786be74ba11f072ce80889
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-10-20 00:32:57 +0100

    Add basic tests Just copied code from sample1 and turned it into a Unit test

--- test/basic_tests.cpp
+++ test/basic_tests.cpp
@@ -2,5 +2,16 @@
 #include <duckx.hpp>
 
 TEST(basic_test, test1) {
+    duckx::Document doc("my_test.docx");
+    doc.open();
 
+    std::ostringstream ss;
+
+    for (auto p = doc.paragraphs(); p.has_next(); p.next()) {
+        for (auto r = p.runs(); r.has_next(); r.next()) {
+            ss << r.get_text() << std::endl;
+        }
+    }
+
+    ASSERT_EQ("This is a test\nokay?\n",ss.str());
 }
