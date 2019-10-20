26e9e182073114ad9ca042c88539b937c7cc437e
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-10-20 20:17:03 +0100

    Add unit testing using doctest

---
+++ test/basic_tests.cpp
@@ -0,0 +1,20 @@
+#include <sstream>
+#include <duckx.hpp>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("checks contents of my_test.docx") {
+    duckx::Document doc("my_test.docx");
+    doc.open();
+
+    std::ostringstream ss;
+
+    for (duckx::Paragraph p = doc.paragraphs(); p.has_next(); p.next()) {
+        for (duckx::Run r = p.runs(); r.has_next(); r.next()) {
+            ss << r.get_text() << std::endl;
+        }
+    }
+
+    CHECK_EQ("This is a test\nokay?\n", ss.str());
+}
