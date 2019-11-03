f82e9d704371224c0aa7faca3c4ef556b84e7e52
Cihan Sari <cihansari86@gmail.com>
2019-11-03 17:17:18 +0100

    Added begin/end pair for MyTestObject to differentiate the parent / current types from xml_node.

--- test/iterator_tests.cpp
+++ test/iterator_tests.cpp
@@ -7,45 +7,53 @@
 
 TEST_CASE("checks contents of my_test.docx with iterator")
 {
-	duckx::Document doc("my_test.docx");
-	doc.open();
+    duckx::Document doc("my_test.docx");
+    doc.open();
 
-	std::ostringstream ss;
+    std::ostringstream ss;
 
-	for (duckx::Paragraph p : doc.paragraphs())
-	{
-		for (duckx::Run r : p.runs())
-		{
-			ss << r.get_text() << std::endl;
-			//std::puts(r.get_text().c_str());
-		}
-	}
-	std::puts(ss.str().c_str());
-	CHECK_EQ("This is a test\nokay?\n", ss.str());
+    for (duckx::Paragraph p : doc.paragraphs())
+    {
+        for (duckx::Run r : p.runs())
+        {
+            ss << r.get_text() << std::endl;
+            //std::puts(r.get_text().c_str());
+        }
+    }
+    std::puts(ss.str().c_str());
+    CHECK_EQ("This is a test\nokay?\n", ss.str());
 }
 
 namespace duckx {
 struct MyTestObject final {
-	int current = 42;
-	int parent = 1;
-	int j = 86;
-	MyTestObject(int parent, int current) : parent(parent), current(current){}
-	MyTestObject() = default;
-	MyTestObject& next() { ++current; return *this;}
-	bool has_next() const { return current!=j;}
-	bool operator== (MyTestObject const& other) const {return other.current==current && other.j==j;}
+    int current = 42;
+    int parent = 1;
+    int j = 86;
+    MyTestObject(int parent, int current) : parent(parent), current(current){}
+    MyTestObject() = default;
+    MyTestObject& next() { ++current; return *this;}
+    bool has_next() const { return current!=j;}
+    bool operator== (MyTestObject const& other) const {return other.current==current && other.j==j;}
 };
+// Entry point
+auto begin(MyTestObject const& obj) -> Iterator<MyTestObject, int> {
+  return Iterator<MyTestObject, int, int>(obj.parent, obj.current);
+}
+
+auto end(MyTestObject const& obj) -> Iterator<MyTestObject, int> {
+  return Iterator<MyTestObject, int, int>(obj.parent, static_cast<decltype(obj.current)>(0));
+}
 }  // namespace duckx
 
 TEST_CASE("Check equality in")
 {
-	auto const testObject = duckx::MyTestObject{};
+    auto const testObject = duckx::MyTestObject{};
 	auto p1 = begin(testObject);
-	auto p2 = begin(testObject);
-	CHECK_EQ(p1, p2);
-	duckx::Document doc("my_test.docx");
-	doc.open();
+    auto p2 = begin(testObject);
+    CHECK_EQ(p1, p2);
+    duckx::Document doc("my_test.docx");
+    doc.open();
 
-	CHECK_EQ(begin(doc.paragraphs()), begin(doc.paragraphs()));
-	
+    CHECK_EQ(begin(doc.paragraphs()), begin(doc.paragraphs()));
+    
 }
