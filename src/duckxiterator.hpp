1999c094f3c2b34d7d532cfac46955f7302778d2
Cihan Sari <cihansari86@gmail.com>
2019-11-03 17:16:36 +0100

    Removed type access through decltype inside the Iterator class using private members. Instead made the Parent and Current types template arguments. Access to private types is permitted on some compilers (e.g. msvc) and forbidden on others (GNU).

--- src/duckxiterator.hpp
+++ src/duckxiterator.hpp
@@ -7,81 +7,78 @@
 #ifndef DUCKXITERATOR_H
 #define DUCKXITERATOR_H
 
-namespace duckx
-{
-	template <class T>
-	class Iterator {
-	private:
-		using ParentType = decltype(T::parent);
-		using CurrentType = decltype(T::current);
-        ParentType parent{0};
-        CurrentType current{0};
-		mutable T buffer{};
+namespace pugi {class xml_node;}
 
-	public:
-		Iterator() = default;
-
-		Iterator(ParentType parent, CurrentType current) : parent(parent), current(current)
-		{
-		}
-
-		bool operator!=(const Iterator& other) const
-		{
-			return parent != other.parent || current != other.current;
-		}
-
-		bool operator==(const Iterator& other) const
-		{
-			return !this->operator!=(other);
-		}
-
-		Iterator& operator++() { 
-			this->current = this->current.next_sibling();
-			return *this;
-		}
-
-		auto operator*() const -> T const& { 
-			// Only update the buffer when the user wants to accces to the data
-			buffer.set_parent(parent);
-			buffer.set_current(current);
-			return buffer; 
-		}
-
-		auto operator->() const -> T const* { 
-			return &(this->operator*()); 
-		}
-	};
-
-	class IteratorHelper {
+namespace duckx {
+    template <class T, class P, class C = P>
+    class Iterator {
     private:
-        template <class T>
-        static Iterator<T> make_begin(T const& obj) {
-	        return Iterator<T>(obj.parent, obj.current);
+        using ParentType = P;
+        using CurrentType = C;
+        ParentType parent{ 0 };
+        CurrentType current{ 0 };
+        mutable T buffer{};
+
+    public:
+        Iterator() = default;
+
+        Iterator(ParentType parent, CurrentType current)
+            : parent(parent), current(current) {}
+
+        bool operator!=(const Iterator& other) const
+        {
+            return parent != other.parent || current != other.current;
         }
 
-  		template <class T>
-        static Iterator<T> make_end(T const& obj) {
-            return Iterator<T>(obj.parent, static_cast<decltype(obj.current)>(0));
+        bool operator==(const Iterator& other) const
+        {
+            return !this->operator!=(other);
+        }
+
+        Iterator& operator++() {
+            this->current = this->current.next_sibling();
+            return *this;
+        }
+
+        auto operator*() const -> T const& {
+            // Only update the buffer when the user wants to accces to the data
+            buffer.set_parent(parent);
+            buffer.set_current(current);
+            return buffer;
+        }
+
+        auto operator-> () const -> T const* { return &(this->operator*()); }
+    };
+
+    class IteratorHelper {
+    private:
+        using P = pugi::xml_node;
+        template <class T>
+        static auto make_begin(T const& obj) -> Iterator<T, P> {
+            return Iterator<T, P>(obj.parent, obj.current);
         }
 
         template <class T>
-        friend Iterator<T> begin(T const&);
+        static auto make_end(T const& obj) -> Iterator<T, P> {
+            return Iterator<T, P>(obj.parent, static_cast<decltype(obj.current)>(0));
+        }
+
         template <class T>
-        friend Iterator<T> end(T const&);
-	};
+        friend auto begin(T const&)->Iterator<T, P>;
+        template <class T>
+        friend auto end(T const&)->Iterator<T, P>;
+    };
 
-	// Entry point
-	template <class T>
-	auto begin(T const& obj) -> Iterator<T>
-	{
-          return IteratorHelper::make_begin(obj);
-	}
-
-	template <class T>
-	auto end(T const& obj) -> Iterator<T>
-	{
-          return IteratorHelper::make_end(obj);
+    // Entry point
+    template <class T>
+    auto begin(T const& obj) -> Iterator<T, pugi::xml_node> {
+        return IteratorHelper::make_begin(obj);
     }
-} // namespace duckx
+
+    template <class T>
+    auto end(T const& obj) -> Iterator<T, pugi::xml_node> {
+        return IteratorHelper::make_end(obj);
+    }
+}  // namespace duckx
 
 #endif
