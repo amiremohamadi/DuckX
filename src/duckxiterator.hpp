8b902a030e22b4a578a5c4d85c9134534d02173f
AMIR <31338382+amiremohamadi@users.noreply.github.com>
2019-11-05 23:26:36 +0330

    re-format code style

--- src/duckxiterator.hpp
+++ src/duckxiterator.hpp
@@ -1,6 +1,6 @@
 /*
  * Under MIT license
- * Author: Amir Mohamadi (@amiremohamadi)
+ * Author: Cihan SARI (@CihanSari)
  * DuckX is a free library to work wirh docx files.
  */
 
@@ -25,58 +25,56 @@ namespace duckx {
         Iterator(ParentType parent, CurrentType current)
             : parent(parent), current(current) {}
 
-        bool operator!=(const Iterator& other) const
-        {
+        bool operator!=(const Iterator &other) const {
             return parent != other.parent || current != other.current;
         }
 
-        bool operator==(const Iterator& other) const
-        {
+        bool operator==(const Iterator &other) const {
             return !this->operator!=(other);
         }
 
-        Iterator& operator++() {
+        Iterator &operator++() {
             this->current = this->current.next_sibling();
             return *this;
         }
 
-        auto operator*() const -> T const& {
+        auto operator*() const -> T const & {
             // Only update the buffer when the user wants to accces to the data
             buffer.set_parent(parent);
             buffer.set_current(current);
             return buffer;
         }
 
-        auto operator-> () const -> T const* { return &(this->operator*()); }
+        auto operator-> () const -> T const * { return &(this->operator*()); }
     };
 
     class IteratorHelper {
     private:
         using P = pugi::xml_node;
         template <class T>
-        static auto make_begin(T const& obj) -> Iterator<T, P> {
+        static auto make_begin(T const &obj) -> Iterator<T, P> {
             return Iterator<T, P>(obj.parent, obj.current);
         }
 
         template <class T>
-        static auto make_end(T const& obj) -> Iterator<T, P> {
+        static auto make_end(T const &obj) -> Iterator<T, P> {
             return Iterator<T, P>(obj.parent, static_cast<decltype(obj.current)>(0));
         }
 
         template <class T>
-        friend auto begin(T const&)->Iterator<T, P>;
+        friend auto begin(T const &)->Iterator<T, P>;
         template <class T>
-        friend auto end(T const&)->Iterator<T, P>;
+        friend auto end(T const &)->Iterator<T, P>;
     };
 
     // Entry point
     template <class T>
-    auto begin(T const& obj) -> Iterator<T, pugi::xml_node> {
+    auto begin(T const &obj) -> Iterator<T, pugi::xml_node> {
         return IteratorHelper::make_begin(obj);
     }
 
     template <class T>
-    auto end(T const& obj) -> Iterator<T, pugi::xml_node> {
+    auto end(T const &obj) -> Iterator<T, pugi::xml_node> {
         return IteratorHelper::make_end(obj);
     }
 }  // namespace duckx
