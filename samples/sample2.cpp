8ff6bae04d8a045519f58a5f8835616baa0efa5b
amir <amiremohamadi@yahoo.com>
2019-12-23 14:00:27 +0330

    update sample2.cpp

--- samples/sample2.cpp
+++ samples/sample2.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include "../src/duckx.hpp"
 using namespace std;
-using namespace duckx; 
 
 int main() {
     duckx::Document doc("my_test.docx");
     doc.open();
 
     duckx::Paragraph p = doc.paragraphs().insert_paragraph_after("You can insert text in ");
-    p.add_run("italic, ", Run::Italic);
-    p.add_run("bold, ", Run::Bold);
-    p.add_run("underline, ", Run::Underline);
-    p.add_run("strikethrough, ", Run::Strikethrough);
-    p.add_run("superscript", Run::Superscript);
+    p.add_run("italic, ", duckx::none);
+    p.add_run("bold, ", duckx::bold);
+    p.add_run("underline, ", duckx::strikethrough);
+    p.add_run("superscript", duckx::superscript);
     p.add_run(" or ");
-    p.add_run("subscript, ", Run::Subscript);
-    p.add_run("small caps, ", Run::SmallCaps);
-    p.add_run("and shadows, ", Run::Shadow);
+    p.add_run("subscript, ", duckx::subscript);
+    p.add_run("small caps, ", duckx::smallcaps);
+    p.add_run("and shadows, ", duckx::shadow);
     p.add_run("and of course ");
-    p.add_run("combine them.", Run::Bold | Run::Italic | duckx::Run::Underline | Run::SmallCaps);
+    p.add_run("combine them.", duckx::bold | duckx::italic | duckx::underline | duckx::smallcaps);
     
     doc.save();
 
