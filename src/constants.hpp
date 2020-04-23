44464cbe96ac85983274e5e254b07e4ea9d9b9c8
Nikolas Klauser <nikolasklauser@berlin.de>
2020-04-23 23:25:23 +0200

    Added clang-format file

--- src/constants.hpp
+++ src/constants.hpp
@@ -2,18 +2,18 @@
 #define CONSTANTS_HPP
 
 namespace duckx {
-    typedef unsigned const int formatting_flag;
+typedef unsigned const int formatting_flag;
 
-    // text-formatting flags
-    formatting_flag none          = 0;
-    formatting_flag bold          = 1 << 0;
-    formatting_flag italic        = 1 << 1;
-    formatting_flag underline     = 1 << 2;
-    formatting_flag strikethrough = 1 << 3;
-    formatting_flag superscript   = 1 << 4;
-    formatting_flag subscript     = 1 << 5;
-    formatting_flag smallcaps     = 1 << 6;
-    formatting_flag shadow        = 1 << 7;
-}
+// text-formatting flags
+formatting_flag none = 0;
+formatting_flag bold = 1 << 0;
+formatting_flag italic = 1 << 1;
+formatting_flag underline = 1 << 2;
+formatting_flag strikethrough = 1 << 3;
+formatting_flag superscript = 1 << 4;
+formatting_flag subscript = 1 << 5;
+formatting_flag smallcaps = 1 << 6;
+formatting_flag shadow = 1 << 7;
+} // namespace duckx
 
 #endif
--- src/duckx.hpp
+++ src/duckx.hpp
@@ -2,153 +2,156 @@
  * Under MIT license
  * Author: Amir Mohamadi (@amiremohamadi)
  * DuckX is a free library to work with docx files.
-*/
+ */
 
 #ifndef DUCKX_H
 #define DUCKX_H
 
-#include <stdlib.h>
 #include <cstdio>
+#include <stdlib.h>
 #include <string>
 
-#include "pugixml.hpp"
 #include "constants.hpp"
+#include "pugixml.hpp"
 #include "zip.h"
-
 
 // TODO: Use container-iterator design pattern!
 
 namespace duckx {
-    // Run contains runs in a paragraph
-    class Run {
-    private:
-        friend class IteratorHelper;
-        // Store the parent node (a paragraph)
-        pugi::xml_node parent;
-        // And store current node also
-        pugi::xml_node current;
+// Run contains runs in a paragraph
+class Run {
+  private:
+    friend class IteratorHelper;
+    // Store the parent node (a paragraph)
+    pugi::xml_node parent;
+    // And store current node also
+    pugi::xml_node current;
 
-    public:
-        Run();
-        Run(pugi::xml_node, pugi::xml_node);
-        void set_parent(pugi::xml_node);
-        void set_current(pugi::xml_node);
+  public:
+    Run();
+    Run(pugi::xml_node, pugi::xml_node);
+    void set_parent(pugi::xml_node);
+    void set_current(pugi::xml_node);
 
-        std::string get_text() const;
-        bool set_text(const std::string&) const;
-        bool set_text(const char *) const;
+    std::string get_text() const;
+    bool set_text(const std::string &) const;
+    bool set_text(const char *) const;
 
-        Run &next();
-        bool has_next() const;
-    };
+    Run &next();
+    bool has_next() const;
+};
 
-    // Paragraph contains a paragraph
-    // and stores runs
-    class Paragraph {
-    private:
-        friend class IteratorHelper;
-		// Store parent node (usually the body node)
-        pugi::xml_node parent;
-        // And store current node also
-        pugi::xml_node current;
-        // A paragraph consists of runs
-        Run run;
+// Paragraph contains a paragraph
+// and stores runs
+class Paragraph {
+  private:
+    friend class IteratorHelper;
+    // Store parent node (usually the body node)
+    pugi::xml_node parent;
+    // And store current node also
+    pugi::xml_node current;
+    // A paragraph consists of runs
+    Run run;
 
-    public:
-        Paragraph();
-        Paragraph(pugi::xml_node, pugi::xml_node);
-        void set_parent(pugi::xml_node);
-        void set_current(pugi::xml_node);
+  public:
+    Paragraph();
+    Paragraph(pugi::xml_node, pugi::xml_node);
+    void set_parent(pugi::xml_node);
+    void set_current(pugi::xml_node);
 
-        Paragraph &next();
-        bool has_next() const;
+    Paragraph &next();
+    bool has_next() const;
 
-        Run &runs();
-        Run &add_run(const std::string&, duckx::formatting_flag = duckx::none);
-        Run &add_run(const char*, duckx::formatting_flag = duckx::none);
-        Paragraph &insert_paragraph_after(const std::string&, duckx::formatting_flag = duckx::none);
-    };
+    Run &runs();
+    Run &add_run(const std::string &, duckx::formatting_flag = duckx::none);
+    Run &add_run(const char *, duckx::formatting_flag = duckx::none);
+    Paragraph &insert_paragraph_after(const std::string &,
+                                      duckx::formatting_flag = duckx::none);
+};
 
-	// TableCell contains one or more paragraphs
-	class TableCell {
-	private:
-        friend class IteratorHelper;
-		pugi::xml_node parent;
-		pugi::xml_node current;
+// TableCell contains one or more paragraphs
+class TableCell {
+  private:
+    friend class IteratorHelper;
+    pugi::xml_node parent;
+    pugi::xml_node current;
 
-		Paragraph paragraph;
-	public:
-		TableCell();
-		TableCell(pugi::xml_node, pugi::xml_node);
+    Paragraph paragraph;
 
-		void set_parent(pugi::xml_node);
-		void set_current(pugi::xml_node);
+  public:
+    TableCell();
+    TableCell(pugi::xml_node, pugi::xml_node);
 
-		Paragraph& paragraphs();
+    void set_parent(pugi::xml_node);
+    void set_current(pugi::xml_node);
 
-		TableCell& next();
-		bool has_next() const;
-	};
+    Paragraph &paragraphs();
 
-	// TableRow consists of one or more TableCells
-	class TableRow {
-        friend class IteratorHelper;
-		pugi::xml_node parent;
-		pugi::xml_node current;
+    TableCell &next();
+    bool has_next() const;
+};
 
-		TableCell cell;
-	public:
-		TableRow();
-		TableRow(pugi::xml_node, pugi::xml_node);
-		void set_parent(pugi::xml_node);
-		void set_current(pugi::xml_node);
+// TableRow consists of one or more TableCells
+class TableRow {
+    friend class IteratorHelper;
+    pugi::xml_node parent;
+    pugi::xml_node current;
 
-		TableCell& cells();
+    TableCell cell;
 
-		bool has_next() const;
-		TableRow& next();
-	};
+  public:
+    TableRow();
+    TableRow(pugi::xml_node, pugi::xml_node);
+    void set_parent(pugi::xml_node);
+    void set_current(pugi::xml_node);
 
-	// Table consists of one or more TableRow objects
-	class Table {
-	private:
-        friend class IteratorHelper;
-		pugi::xml_node parent;
-		pugi::xml_node current;
+    TableCell &cells();
 
-		TableRow row;
-	public:
-		Table();
-		Table(pugi::xml_node, pugi::xml_node);
-		void set_parent(pugi::xml_node);
-		void set_current(pugi::xml_node);
+    bool has_next() const;
+    TableRow &next();
+};
 
-		Table& next();
-		bool has_next() const;
+// Table consists of one or more TableRow objects
+class Table {
+  private:
+    friend class IteratorHelper;
+    pugi::xml_node parent;
+    pugi::xml_node current;
 
-		TableRow& rows();
-	};
+    TableRow row;
 
-    // Document contains whole the docx file
-    // and stores paragraphs
-    class Document {
-    private:
-        friend class IteratorHelper;
-        std::string directory;
-        Paragraph paragraph;
-		Table table;
-        pugi::xml_document document;
+  public:
+    Table();
+    Table(pugi::xml_node, pugi::xml_node);
+    void set_parent(pugi::xml_node);
+    void set_current(pugi::xml_node);
 
-    public:
-        Document();
-        Document(std::string);
-        void file(std::string);
-        void open();
-        void save() const;
+    Table &next();
+    bool has_next() const;
 
-        Paragraph &paragraphs();
-		Table& tables();
-    };
-}
+    TableRow &rows();
+};
+
+// Document contains whole the docx file
+// and stores paragraphs
+class Document {
+  private:
+    friend class IteratorHelper;
+    std::string directory;
+    Paragraph paragraph;
+    Table table;
+    pugi::xml_document document;
+
+  public:
+    Document();
+    Document(std::string);
+    void file(std::string);
+    void open();
+    void save() const;
+
+    Paragraph &paragraphs();
+    Table &tables();
+};
+} // namespace duckx
 
 #endif
