b14787bc08180a2301ad0b80476eb949060eb755
amiremohamadi <amiremohamadi@yahoo.com>
2020-05-15 12:46:54 +0430

    [refactor] fix sample includes due to new structure

--- samples/sample2.cpp
+++ samples/sample2.cpp
@@ -1,12 +1,13 @@
+#include <duckx.hpp>
 #include <iostream>
-#include "../src/duckx.hpp"
 using namespace std;
 
 int main() {
     duckx::Document doc("my_test.docx");
     doc.open();
 
-    duckx::Paragraph p = doc.paragraphs().insert_paragraph_after("You can insert text in ");
+    duckx::Paragraph p =
+        doc.paragraphs().insert_paragraph_after("You can insert text in ");
     p.add_run("italic, ", duckx::none);
     p.add_run("bold, ", duckx::bold);
     p.add_run("underline, ", duckx::strikethrough);
@@ -16,8 +17,9 @@ int main() {
     p.add_run("small caps, ", duckx::smallcaps);
     p.add_run("and shadows, ", duckx::shadow);
     p.add_run("and of course ");
-    p.add_run("combine them.", duckx::bold | duckx::italic | duckx::underline | duckx::smallcaps);
-    
+    p.add_run("combine them.", duckx::bold | duckx::italic | duckx::underline |
+                                   duckx::smallcaps);
+
     doc.save();
 
     return 0;
