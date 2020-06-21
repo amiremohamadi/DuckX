e4ac4135f17f7c6cfd8eccbd6e68675f32b88e7d
amiremohamadi <amiremohamadi@yahoo.com>
2020-06-22 00:46:35 +0430

    seperate run to two distinct files

---
+++ include/run.hh
@@ -0,0 +1,41 @@
+/*
+ * Under MIT license
+ * Author: Amir Mohamadi (@amiremohamadi)
+ * DuckX is a free library to work with docx files.
+ */
+
+#ifndef RUN_HH_
+#define RUN_HH_
+
+namespace duckx {
+class Run {
+    // wrapping <w:r> element
+  private:
+    friend class IteratorHelper;
+    // store parent and current node to be able to navigate whole the tree
+    pugi::xml_node parent;
+    pugi::xml_node current;
+
+    // constructs attribute from xml_node
+    Run(pugi::xml_node parent, pugi::xml_node current);
+
+  public:
+    // default constructor. constructs an empty attribute
+    Run();
+
+    // get attribute text
+    std::string get_text() const;
+
+    // set attribute text
+    bool set_text(const std::string &text) const;
+    bool set_text(const char *text) const;
+
+    // get next attribute
+    Run &next();
+
+    // check if attribute has next
+    bool has_next() const;
+};
+} // namespace duckx
+
+#endif // END RUN_HH_
---
+++ src/run.cc
@@ -0,0 +1,40 @@
+/*
+ * Under MIT license
+ * author : Amir Mohamadi (@amiremohamadi)
+ * this file contains all implementation stuffs related to run.hh
+ */
+
+#include <duckx/run.hh>
+
+// constructs attribute from xml_node
+duckx::Run::Run(pugi::xml_node parent, pugi::xml_node current) {
+    this->set_parent(parent);
+    this->set_current(current);
+}
+
+// default constructor
+duckx::Run::Run() {}
+
+// get attribute text
+std::string duckx::Run::get_text() const {
+    return this->current.child("w:t").text().get();
+}
+
+// set attribute text (std::string)
+bool duckx::Run::set_text(const std::string &text) const {
+    return this->current.child("w:t").text().set(text.c_str());
+}
+
+// set attribute text (c_str)
+bool duckx::Run::set_text(const char *text) const {
+    return this->current.child("w:t").text().set(text);
+}
+
+// get next attribute
+duckx::Run &duckx::Run::next() {
+    this->current = this->current.next_sibling();
+    return *this;
+}
+
+// check if attribute has next
+bool duckx::Run::has_next() const { return this->current != 0; }
