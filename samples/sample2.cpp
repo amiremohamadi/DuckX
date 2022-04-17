fc1163738e099bf1b2859b48bc8ef73aca179d21
zjp369 <sun_xf@tom.com>
2022-04-17 11:53:18 +0800

    Test added api

--- samples/sample2.cpp
+++ samples/sample2.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main() {
-    duckx::Document doc("mm.docx");
+    duckx::Document doc("my_test.docx");
     doc.open();
 
-//     duckx::Paragraph p =
-//         doc.paragraphs().insert_paragraph_after("You can insert text in ");
-//     p.add_run("italic, ", duckx::none);
-//     p.add_run("bold, ", duckx::bold);
-//     p.add_run("underline, ", duckx::strikethrough);
-//     p.add_run("superscript", duckx::superscript);
-//     p.add_run(" or ");
-//     p.add_run("subscript, ", duckx::subscript);
-//     p.add_run("small caps, ", duckx::smallcaps);
-//     p.add_run("and shadows, ", duckx::shadow);
-//     p.add_run("and of course ");
-//     p.add_run("combine them.", duckx::bold | duckx::italic | duckx::underline |
-//                                    duckx::smallcaps);
+    duckx::Paragraph p =
+        doc.paragraphs().insert_paragraph_after("You can insert text in ");
+    p.add_run("italic, ", duckx::none);
+    p.add_run("bold, ", duckx::bold);
+    p.add_run("underline, ", duckx::strikethrough);
+    p.add_run("superscript", duckx::superscript);
+    p.add_run(" or ");
+    p.add_run("subscript, ", duckx::subscript);
+    p.add_run("small caps, ", duckx::smallcaps);
+    p.add_run("and shadows, ", duckx::shadow);
+    p.add_run("and of course ");
+    p.add_run("combine them.", duckx::bold | duckx::italic | duckx::underline |
+                                   duckx::smallcaps);
 	doc.clear();
-	doc.paragraphs().append().add_run("tttt");
-	//doc.tables().append();
+	doc.paragraphs().append().add_run("Empty docx!");
+	doc.tables().append();
+
     doc.save();
 
     return 0;
