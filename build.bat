161beca347f0d95c855bd11015dced7a41ee0d24
zjp <605146581@qq.com>
2022-05-09 20:54:53 +0800

    增加编译静态库和动态库批处理

---
+++ build.bat
@@ -0,0 +1,9 @@
+@setlocal
+call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+
+mkdir c:\r
+mkdir b
+cd b
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=c:\r -DBUILD_SAMPLES:BOOL=ON -DCMAKE_DEBUG_POSTFIX:STRING="D" ..
+devenv.com duckx.sln /build "Debug|x64" /project INSTALL.vcxproj
+devenv.com duckx.sln /build "Release|x64" /project INSTALL.vcxproj
